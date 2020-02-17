70e9ab2d661c323d9b7d0d64527b2e6e6cc2503a
leonardocunha2107 <leonardocunha2107@gmail.com>
2020-02-17 16:02:00 +0100

    debug endgame

--- cli_game.py
+++ cli_game.py
@@ -1,5 +1,6 @@
 from game import SnakeGame
 from os import system,name
+import matplotlib.pyplot as plt
 
 def clear(): 
    ##Thanks stackoverflow
@@ -14,12 +15,14 @@ def clear():
 if __name__=='__main__':
     game=SnakeGame()
     while (True):
-        print("{}\n\n".format(game))
+        plt.imshow(game.render())
+        plt.show()
         move=input("Move:\n")
         if move=='exit':
             sys.exit(0)
         move=int(move)
         if move in range(4):
-            _,_,done,_=game.step(action)
+            _,_,done,_=game.step(move)
+        print(done)
             
             
--- game.py
+++ game.py
@@ -1,10 +1,14 @@
-import torch
 import numpy as np
 from collections import deque
 import gym
 import random
 
-directions=[(1,0),(-1,0),(0,1),(0,-1)]
+BLACK     = ( 17,  18,  13)
+RED       = (255,   0,   0)
+GREEN     = (  0, 255,   0)
+WHITE     = (255, 255, 255)
+#directions=[(1,0),(-1,0),(0,1),(0,-1)]
+directions=[(0,1),(-1,0),(0,-1),(1,0)]
 """directions=['RIGHT','LEFT','UP','DOWN']
 d2v={d:v for d,v in zip(directions,[(1,0),(-1,0),(0,1),(0,-1)])}
 """
@@ -35,15 +39,18 @@ class SnakeGame(gym.Env):
     
     def random_pos(self):
         pos=random.sample(self.empty,1)
-        self.empty.pop(pos)
-        return pos
+        self.empty.remove(pos[0])
+        return pos[0]
 
     def step(self,action):
         snake_head=self.snake.pop()
         self.snake.append(snake_head)
-        snake_head=((snake_head[i]+directions[action][i])%self.dim[i] for i in [0,1])
+        print(snake_head)
+        snake_head=tuple(((snake_head[i]+directions[action][i])%self.dim[i] for i in [0,1]))
         
+        print(snake_head)
         if  snake_head!=self.fruit and snake_head not in self.empty:
+
             return self.board,-1.,True,{}
         
         self.snake.append(snake_head)
@@ -58,12 +65,37 @@ class SnakeGame(gym.Env):
         self.board[snake_tail]=0
         
         return self.board,0,False,{}
+    """
+        renders the game.
+        Creates a rgb array with the colors corresponding to the current state of the game
+        
+        @:parameter mode mode of render request
+                         'rgb' : @return array of rgb
+    """
+    def render(self, mode = 'rgb'):
+        if mode =='rgb':
+            #grid = np.zeros((self.dim[0],self.dim[1],3))
+            grid = np.zeros((self.dim[0],self.dim[1]), dtype = (int,3))
+            
+            for i in range(self.dim[0]):
+                for j in range(self.dim[1]):
+                    if self.board[i,j] == self.SNAKE:
+                        grid[i,j] = np.array(GREEN)
+                    elif self.board[i,j] == self.FRUIT:
+                        grid[i,j] = np.array(RED)
+            
+            #We use a different color to the snake's head:
+            head = self.snake.pop()
+            print(head)
+            grid[head[0],head[1]] = np.array(WHITE)
+            self.snake.append(head)
+
+            return grid
+        else:
+            raise('error: function not implemented')
+            return -1
         
         
-    
-        
-        
-   
-                
+
             
         
